075d71ed2344213a4e7e4cf497469e9f30ba6d44
Kaleb McKone <20476319+krmckone@users.noreply.github.com>
2024-02-15 11:39:54 -0500

    Fix degree to radian function

--- rtweekend.h
+++ rtweekend.h
@@ -14,7 +14,7 @@ const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
-  return degrees * pi / 100.0;
+  return degrees * pi / 180.0;
 }
 
 inline double random_double() {
